8ff27f22c4ab23b43fc2d7c579c299a6f2cdacb7
shaila <shailacdi@gmail.com>
2018-09-23 02:46:20 -0400

    Updated database related files

--- setup/cassandra/create_cassandra_tables.py
+++ setup/cassandra/create_cassandra_tables.py
@@ -10,3 +10,9 @@ session.execute("create keyspace if not exists "+ keyspace_name+ " WITH REPLICAT
 print("use "+keyspace_name)
 session.execute("use {0}".format(keyspace_name))
 session.execute("CREATE TABLE if not exists trip_stats (time_block int, month text,day text, borough_code int, borough_name text,std_dev int, mean int, PRIMARY KEY (borough_code,time_block, month, day))")
+
+session.execute("CREATE TABLE if not exists medallion_master (medallion_id text, PRIMARY KEY (medallion_id))")
+
+session.execute("CREATE TABLE if not exists driver_master (driver_id int, driver_rating int, preferred_borough int, active int, PRIMARY KEY (driver_id))")
+
+session.execute("CREATE TABLE if not exists driver_assignment (assign_date text, driver_id int, medallion_id, PRIMARY KEY (assign_date, driver_id))")
