7cd37d448528c73b78daac62173420b4944f0f3a
shaila <shailacdi@gmail.com>
2018-10-03 15:55:58 -0400

    Minor changes

--- setup/cassandra/create_cassandra_tables.py
+++ setup/cassandra/create_cassandra_tables.py
@@ -9,24 +9,9 @@ session = cluster.connect()
 session.execute("create keyspace if not exists "+ keyspace_name+ " WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 }")
 print("use "+keyspace_name)
 session.execute("use {0}".format(keyspace_name))
-session.execute("CREATE TABLE if not exists trip_stats (time_block int, month text,day text, borough_code int, borough_name text,std_dev int, mean int, PRIMARY KEY (borough_code,time_block, month, day))")
 
-session.execute("CREATE TABLE if not exists medallion_master (medallion_id text, PRIMARY KEY (medallion_id))")
+session.execute("CREATE TABLE if not exists trip_stats (time_block int, month text,day text, borough_code int, borough_name text, mean int, PRIMARY KEY (borough_code,time_block, month, day))")
 
-session.execute("CREATE TABLE if not exists driver_details (driver_id int, driver_lname text, driver_fname text,driver_rating float, start_borough_id int, active int, current_borough_id int, PRIMARY KEY (driver_id))")
+session.execute("CREATE TABLE if not exists real_trip (assign_date text, time_block int, month text, day text, borough_code int, borough_name text,PRIMARY KEY (assign_date))")
 
-session.execute("CREATE TABLE if not exists medallion_driver_assignment (assign_date text, hack_license text, medallion_id text, lat float, long float, PRIMARY KEY (assign_date, driver_id))")
-
-session.execute("CREATE TABLE if not exists real_trip (assign_date text, hack_license text, medallion_id text, time_block int, month text, day text, borough_code int, borough_name text, lat float, long float, PRIMARY KEY (assign_date, hack_license))")
-
-session.execute("CREATE TABLE if not exists real_trip_stats (assign_date text, time_block int, month text,day text, borough_code int, borough_name text,actual_trips int, mean int, std_dev int, PRIMARY KEY (assign_date,borough_code,time_block, month, day))")
-
-
-
-
-"""
-CREATE TABLE if not exists medallion_master (medallion_id text, PRIMARY KEY (medallion_id))
-CREATE TABLE if not exists driver_details (driver_id int, driver_rating int, start_borough_id int, active int, current_borough_id int, PRIMARY KEY (driver_id))
-CREATE TABLE if not exists medallion_driver_assignment (assign_date text, driver_id int, medallion_id text, PRIMARY KEY (assign_date, driver_id))
-
-"""
+session.execute("CREATE TABLE if not exists real_trip_stats (time_stamp text, assign_date text, time_block int, month text,day text, borough_code int, borough_name text,actual_trips int, mean int, PRIMARY KEY (time_stamp,assign_date,borough_code,time_block, month, day))")
