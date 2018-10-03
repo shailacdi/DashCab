2c9ed88f531f995cb50a18a4b1995dd1e191f5c4
shaila <shailacdi@gmail.com>
2018-10-03 15:58:34 -0400

    Minor changes

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 CONFIG_FILE=$PWD/config/application.properties
 PFILES=$PWD/src/main/util.py
-FILES=$PWD/data/boroughboundaries.geojson
+SFILES=$PWD/data/taxi_zone_lookup.csv
 CASSANDRA=com.datastax.spark:spark-cassandra-connector_2.11:2.3.2
 SPARK_BATCH_CLUSTER=ec2-35-171-12-195.compute-1.amazonaws.com
 spark-submit --master spark://$SPARK_BATCH_CLUSTER:7077 \
-                 --files $FILES   \
+                 --files $SFILES   \
                  --py-files $PFILES \
                  --packages $CASSANDRA \
 	             --driver-memory 4G \
