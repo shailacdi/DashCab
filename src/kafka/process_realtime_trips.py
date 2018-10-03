c095d30176f53a4ef70deecccf71850d107d814b
shaila <shailacdi@gmail.com>
2018-10-03 15:57:33 -0400

    Minor changes

--- src/kafka/process_realtime_trips.py
+++ src/kafka/process_realtime_trips.py
@@ -3,19 +3,18 @@ This program receives data from the kafka stream, and processes the
 DStreams.The data stream is real time taxi trip requests. The taxi trip request
 has the following format
 
-medallion, hack_license, vendor_id, rate_code, store_and_fwd_flag, pickup_datetime,
-dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance,
-pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude
+VendorID,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,
+RatecodeID,store_and_fwd_flag,PULocationID,DOLocationID
 
 The records are processed using series of spark transformations, actions to compute the
-number of trips per timeblock, joined historical stats to get historical mean and std_dev
+number of trips per timeblock, joined with historical averages under similar conditions,
 and saved into the database.
 
 The following info is saved into the database table real_trips
 "borough_code","time_block","month","day","assign_date","borough_name","actual_trips"
 
 The following info is saved into the database table real_trips_stats
-"assign_date,borough_code,time_block,month,day,actual_trips,borough_name,mean,std_dev int"
+"assign_date,borough_code,time_block,month,day,actual_trips,borough_name,mean"
 
 """
 
@@ -31,6 +30,7 @@ from datetime import datetime
 import calendar
 import pandas as pd
 import util
+from pyspark.sql.functions import rand
 
 def process_stream(rdd, real_trip_table,real_trip_stats_table,keyspace):
     """
@@ -40,28 +40,33 @@ def process_stream(rdd, real_trip_table,real_trip_stats_table,keyspace):
     3. joins the averages from the historical data with #2 above and saves it to the table real_trip_stats
     """
     dataRaw = rdd.map(lambda x: x[1]) \
-                .map(lambda row: util.process_real_trip_record(row,borough_info.value)) \
+                .map(lambda row: util.process_trip_record(row,zone_info.value)) \
                 .filter(lambda row: row != None)
 
     # just flags and enables toDF()
     spark = SparkSession(sc)
     hasattr(dataRaw, "toDF")
 
+    for i in dataRaw.take(10):
+        print i
+
     #save the real trip data into the table real_trip
-    dataRaw.toDF(schema=["assign_date","hack_license","medallion_id","time_block","month","day","borough_code","borough_name","long","lat"],sampleRatio=0.2).write.format("org.apache.spark.sql.cassandra").mode("append").options(table=real_trip_table, keyspace=keyspace).save()
+    dataRaw.toDF(schema=["assign_date","time_block","month","day","borough_code","borough_name"],sampleRatio=0.2).write.format("org.apache.spark.sql.cassandra").mode("append").options(table=real_trip_table, keyspace=keyspace).save()
 
     #get the historical stats broadcast variable
     stats = stats_for_day.value
     stats_df = SQLContext(sc).createDataFrame(stats)
 
     #(key, value) = ((borough_code,time_block,month,day),date,borough_name,1)
-    dataRaw_df=dataRaw.map(lambda x : ((x[6], x[3], x[4], x[5]),(x[0].split(" ")[0], x[7],1 ))) \
+    dataRaw_df=dataRaw.map(lambda x : ((x[4], x[1], x[2], x[3]),(x[0].split(" ")[0], x[5],1 ))) \
                 .reduceByKey(lambda x,y : (x[0],x[1],x[2]+y[2])) \
                 .map(lambda x : (x[0][0],x[0][1],x[0][2],x[0][3],x[1][0],x[1][1],x[1][2])) \
                 .toDF(schema=["borough_code","time_block","month","day","assign_date","borough_name","actual_trips"])
     #join the trip stats with the historical stats and store the consolidate values in table real_trip_stats
-    dataRaw_df.join(stats_df,["borough_code","time_block"]) \
-        .write.format("org.apache.spark.sql.cassandra").mode("append").options(table=real_trip_stats_table, keyspace=keyspace).save()
+    #add a unique row differentiator since every rdd creates the output for which the key may be the same.
+    dataRaw_df = dataRaw_df.join(stats_df,["borough_code","time_block"]) \
+        .withColumn("time_stamp", rand())
+    dataRaw_df.write.format("org.apache.spark.sql.cassandra").mode("append").options(table=real_trip_stats_table, keyspace=keyspace).save()
 
 """
     This is the main method to process real-time trip data. Loads the required properties from, 
@@ -92,7 +97,8 @@ if __name__ == '__main__':
     real_trip_table = properties["cassandra.real_trip_table"]
     real_trip_stats_table = properties["cassandra.real_trip_stats_table"]
     cassandra_keyspace = properties["cassandra.trip.keyspace"]
-    borough_file = properties["nyc_borough"]
+    #borough_file = properties["nyc_borough"]
+    zone_file=properties["nyc_zones"]
 
     #set the spark context with database connection properties
     conf = SparkConf().setAppName("real_trips").set("spark.cassandra.connection.host", cassandra_server)
@@ -101,7 +107,7 @@ if __name__ == '__main__':
     session = cluster.connect()
 
     #load the historical statistics from database based on the day of the week and month
-    query_str = "select time_block, borough_code, mean, std_dev from {0} where day='{1}' and month='{2}' allow filtering".format(trip_stats_table,day,month)
+    query_str = "select time_block, borough_code, mean from {0} where day='{1}' and month='{2}' allow filtering".format(trip_stats_table,day,month)
     session.execute("use {0}".format(cassandra_keyspace))
     stats = session.execute(query_str)
     stats_df = pd.DataFrame(list(stats))
@@ -109,7 +115,7 @@ if __name__ == '__main__':
     stats_for_day = sc.broadcast(stats_df)
 
     #load borough co-ordinates
-    borough_info = sc.broadcast(util.get_borough_data_dict(borough_file))
+    zone_info = sc.broadcast(util.get_zone_dict(zone_file))
 
     #initialize streaming context and create a direct consumer
     ssc = StreamingContext(sc, int(interval))
--- src/kafka/produce_trips.py
+++ src/kafka/produce_trips.py
@@ -1,39 +1,67 @@
+"""
+This program simulates real trip requests. Reads the records from a
+sample trip file stored in s3, performs minimal extraction of fields
+and adds it to the kafka topic.
+Note : since this is a trip dataset thats past, the date of the trip is made
+current while retaining the time (H:M:S) of the original trip records.
+"""
 import sys
-
 import ConfigParser
 import time
 import boto3
 import lazyreader
 from kafka.producer import KafkaProducer
-
-#read the application properties file
-def load_application_properties(env, config_file):
-    props = ConfigParser.RawConfigParser()
-    props.read(config_file)
-    properties = {}
-    for option in props.options(env):
-        properties[option]=props.get(env,option)
-    return properties
-
+import datetime
 
 def produce_msgs(broker_ips, topic, bucket, data_file):
+    """
+    This method produces messages and adds it to the topic
+    """
+    # initialize the producer, s3 connection and get the file from s3 to simulate real trips
     producer = KafkaProducer(bootstrap_servers=broker_ips)
     i = 0
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=bucket, Key=data_file)
+
     while True:
-        s3 = boto3.client('s3')
-        obj = s3.get_object(Bucket=bucket,
-                            Key=data_file)
 
         for line in lazyreader.lazyread(obj['Body'], delimiter='\n'):
             message_info = line.strip()
-            #extract only driver/medallion, pickup lat, pickup long
-            #add borough info
-            if line is not None:
-                producer.send(topic, value=line,key=str(i).encode())
-                print i, line[:20]
-                i = i+1
-            time.sleep(0.01)
+            if not message_info:
+                continue
 
+            line_fields = message_info.split(",")
+            #check if the field is a timestamp
+            try:
+                time.strptime(line_fields[1], '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                continue
+            #manipulating to make the date current however retain the time of the past trip record
+            today = datetime.datetime.now().strftime("%Y-%m-%d")
+            time_stamp = line_fields[1].split(" ")[1]
+            line_fields[1] = today + " " + time_stamp
+            #since trip is just initiated, feed only minimum and relevant info
+            msg = ",".join(line_fields[:9])
+
+            # add the record to the topic with a unique key
+            if msg is not None:
+                producer.send(topic, value=msg,key=str(i).encode())
+                print i, msg
+                i = i+1
+            time.sleep(0.001)
+
+def load_application_properties(env, config_file):
+    """
+    reads the application properties file using a parser and builds a
+    dictionary with key,value pairs
+    """
+    props = ConfigParser.RawConfigParser()
+    props.read(config_file)
+    # builds a dictionary
+    properties = {}
+    for option in props.options(env):
+        properties[option] = props.get(env, option)
+    return properties
 
 # main program
 if __name__ == "__main__":
